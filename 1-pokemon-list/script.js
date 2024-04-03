4c61a06a8f761b8cb9664d94628b0f9596014f36
camillachr <chri.camilla@gmail.com>
2024-04-03 12:43:14 +0200

    Påbegynt filter, eksprimenterer med fetch, ikke komplett

--- 1-pokemon-list/script.js
+++ 1-pokemon-list/script.js
@@ -1,66 +1,123 @@
 const filterContainer = document.querySelector("#filter-container");
+const filterButtons = document.querySelectorAll(".filter-btn");
+const pokemonContainer = document.querySelector("#pokemon-container");
 const savedPokemonContainer = document.querySelector(
   "#saved-pokemon-container"
 );
 let allPokemons = [];
 
-// fetch
+// fetch pokemons
 async function fetchPokemons() {
   try {
-    for (i = 1; i < 51; i++) {
-      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
-      const data = await response.json();
-      const pokemon = data;
-      allPokemons.push(pokemon);
-    }
+    const randomNumber = Math.floor(Math.random() * 1300);
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${randomNumber}`
+    );
+    const data = await response.json();
+    const results = data.results;
 
-    console.log(allPokemons);
-    showAllPokemons();
+    await fetchPokemonDetails(results);
+    updatePokemonList(allPokemons);
   } catch (error) {
     console.error("Kunne ikke hente pokemons", error);
   }
 }
+
 fetchPokemons();
 
-//vis pokemons på siden
-async function showAllPokemons() {
-  const pokemonContainer = document.querySelector("#pokemon-container");
+async function fetchPokemonDetails(results) {
+  try {
+    for (let i = 0; i < results.length; i++) {
+      const pokemonResponse = await fetch(results[i].url);
+      const pokemonData = await pokemonResponse.json();
+
+      let pokemonWithDetails = {
+        name: pokemonData.name,
+        type: pokemonData.types[0].type.name,
+        imgUrl: pokemonData.sprites.front_default,
+      };
+
+      allPokemons.push(pokemonWithDetails);
+    }
+
+    console.log("Ferdig med detaljer", allPokemons);
+  } catch (error) {
+    console.log("Kunne ikke hente pokemon-detaljer", error);
+  }
+}
+
+// vis pokemons på siden
+async function updatePokemonList(pokemonList) {
   pokemonContainer.innerHTML = "";
 
-  for (i = 0; i < allPokemons.length; i++) {
-    //henter informasjon til kort
-    const imgUrl = allPokemons[i].sprites.front_default;
-    const name = allPokemons[i].name;
-    const type = allPokemons[i].types[0].type.name;
-
+  for (i = 0; i < pokemonList.length; i++) {
     //lager kort med styling
     const card = document.createElement("div");
-    card.innerHTML = `<p>${type}</p><img src=" ${imgUrl}" width="100px" /><h3>${name}</h3>  `;
+    card.innerHTML = `<p style="margin: 0;">${pokemonList[i].type}</p><img src=" ${pokemonList[i].imgUrl}" width="100px" /><h3 style="margin: 20px 0;">${pokemonList[i].name}</h3>  `;
     card.style.backgroundColor = "#f6da6c";
     card.style.padding = "20px";
-    card.style.border = "solid 2px white";
+    card.style.boxShadow = "rgba(0, 0, 0, 0.24) 0px 3px 8px";
     card.style.color = "#3a57a1";
     card.style.display = "flex";
     card.style.flexDirection = "column";
     card.style.alignItems = "center";
 
     //legger til knapper på kortet
-    const buttonContainer = document.createElement("div");
     const saveBtn = document.createElement("button");
     const deleteBtn = document.createElement("button");
     const editBtn = document.createElement("button");
+
+    saveBtn.innerHTML = "Lagre";
+    deleteBtn.innerHTML = "Slett";
+    editBtn.innerHTML = "Rediger";
 
     saveBtn.classList.add("btn");
     deleteBtn.classList.add("btn");
     editBtn.classList.add("btn");
 
-    saveBtn.innerHTML = "Lagre";
-    deleteBtn.innerHTML = "Slett";
-    editBtn.innerHTML = "Rediger";
+    const buttonContainer = document.createElement("div");
+    buttonContainer.style.display = "flex";
+    buttonContainer.style.flexDirection = "column";
+    buttonContainer.style.gap = "5px";
 
     //append
     buttonContainer.append(saveBtn, deleteBtn, editBtn);
     card.append(buttonContainer);
     pokemonContainer.append(card);
   }
+}
+
+//filter
+filterButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    //tømme meldingsfeltet
+    const message = document.querySelector("#message");
+    message.innerHTML = "";
+
+    //markere den aktive filter-knappen
+    toggleActiveButton(filterButtons, button);
+
+    //selve filtreringen
+    const type = button.dataset.type;
+    const filteredPokemons = allPokemons.filter(
+      (pokemon) => pokemon.type == type
+    );
+    updatePokemonList(filteredPokemons);
+
+    //Feilhåndtering dersom ingen treff
+    if (filteredPokemons.length == 0) {
+      message.innerHTML = `Beklager, finner ingen Pokemons av typen ${type}.`;
+    }
+  });
+});
+
+//markere aktiv filter-knapp
+function toggleActiveButton(filterButtons, activeButton) {
+  filterButtons.forEach((button) => {
+    if (button === activeButton) {
+      button.classList.add("active");
+    } else {
+      button.classList.remove("active");
+    }
+  });
 }
